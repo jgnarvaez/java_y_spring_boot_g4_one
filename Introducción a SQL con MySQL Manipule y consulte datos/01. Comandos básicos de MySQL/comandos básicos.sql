fb64cacab8ce77b6e6e261b128ac5ba501bafb29
jgnarvaez <jgnarvaez@unicauca.edu.co>
2023-05-16 04:33:38 -0500

    comandos básicos utilizados en MySQL

---
+++ Introducción a SQL con MySQL Manipule y consulte datos/01. Comandos básicos de MySQL/comandos básicos.sql
@@ -0,0 +1,13 @@
+#para usa determinada base de datos
+Use World;
+
+#seleccionar y ver toda la información de una tabla
+select * from city;
+select * from country;
+select * from countryLanguage;
+
+#crear una base de datos
+create database jugos;
+
+#eliminar una base de datos
+drop schema jugos;
